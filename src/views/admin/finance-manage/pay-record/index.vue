5336e400b4e3e0755ef41bdc3f5551989c2d458b
wangzichen <2424015851@qq.com>
2025-05-20 09:27:34 +0800

    fix

--- src/views/admin/finance-manage/pay-record/index.vue
+++ src/views/admin/finance-manage/pay-record/index.vue
@@ -3,7 +3,9 @@
     <div class="header-container">
       <h4>付款记录</h4>
       <div class="button-container">
-        <el-button size="mini" icon="el-icon-plus" type="warning">新增</el-button>
+        <el-button size="mini" icon="el-icon-plus" type="warning" @click="handleAdd"
+          >新增</el-button
+        >
         <el-button size="mini" icon="el-icon-search">查找</el-button>
       </div>
     </div>
@@ -18,47 +20,51 @@
       >
         <el-table-column prop="id" label="付款记录编号" width="110" />
         <el-table-column prop="research_group_account_id" label="账户编号" width="100" />
-        <el-table-column prop="handler" label="经手人" width="90" />
+        <el-table-column prop="handler" label="经手人" width="90">
+          <template slot-scope="scope">
+            {{ scope.row.handler ? scope.row.handler : '无' }}
+          </template>
+        </el-table-column>
         <el-table-column prop="amount" label="金额" sortable width="100">
           <template slot-scope="scope">
             {{ scope.row.amount.toFixed(2) }}
           </template>
         </el-table-column>
-        <el-table-column prop="payMethod" label="支付方式" width="90" />
-        <el-table-column prop="invoiceUrl" label="发票下载地址" width="120">
+        <el-table-column prop="pay_type" label="支付方式" width="90" />
+        <el-table-column prop="invoice_download_address" label="发票下载地址" width="120">
           <template slot-scope="scope">
             <el-link
               type="primary"
-              :href="scope.row.invoiceUrl"
+              :href="scope.row.invoice_download_address"
               target="_blank"
               :underline="false"
-              v-if="scope.row.invoiceUrl"
+              v-if="scope.row.invoice_download_address"
             >
               <i class="el-icon-download"></i> 下载
             </el-link>
             <span v-else>无</span>
           </template>
         </el-table-column>
-        <el-table-column prop="invoiceDate" label="发票日期" width="100">
+        <el-table-column prop="invoice_date" label="发票日期" width="100">
           <template slot-scope="scope">
-            {{ formatDate(scope.row.invoiceDate) }}
+            {{ scope.row.invoice_date ? formatDate(scope.row.invoice_date) : '无' }}
           </template>
         </el-table-column>
-        <el-table-column prop="invoiceImg" label="发票图片附件" width="120">
+        <el-table-column prop="invoice" label="发票图片附件" width="120">
           <template slot-scope="scope">
             <el-image
-              v-if="scope.row.invoiceImg"
+              v-if="scope.row.invoice"
               style="width: 30px; height: 30px"
-              :src="scope.row.invoiceImg"
-              :preview-src-list="[scope.row.invoiceImg]"
+              :src="scope.row.invoice"
+              :preview-src-list="[scope.row.invoice]"
             >
             </el-image>
             <span v-else>无</span>
           </template>
         </el-table-column>
-        <el-table-column prop="receivedTime" label="到账时间" width="110">
+        <el-table-column prop="received_time" label="到账时间" width="110">
           <template slot-scope="scope">
-            {{ formatDate(scope.row.receivedTime) }}
+            {{ scope.row.received_time ? formatDate(scope.row.received_time) : '无' }}
           </template>
         </el-table-column>
         <el-table-column prop="remark" label="备注" width="120" />
@@ -69,12 +75,16 @@
             </el-tag>
           </template>
         </el-table-column>
-        <el-table-column prop="createTime" label="创建时间" width="100">
+        <el-table-column prop="create_time" label="创建时间" width="100">
           <template slot-scope="scope">
-            {{ formatDate(scope.row.createTime) }}
+            {{ scope.row.create_time ? formatDate(scope.row.create_time) : '无' }}
           </template>
         </el-table-column>
-        <el-table-column prop="creator" label="创建人" width="90" />
+        <el-table-column prop="creator" label="创建人" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.create_by }}
+          </template>
+        </el-table-column>
         <el-table-column label="操作" fixed="right">
           <template slot-scope="scope">
             <div class="button-container">
@@ -110,6 +120,64 @@
         :total="total"
         background
       ></el-pagination>
+
+      <!-- 新增付款记录 -->
+      <el-dialog title="新增付款记录" :visible.sync="addDialogVisible" width="50%">
+        <el-form :model="form" label-width="120px">
+          <el-form-item label="账户编号">
+            <el-input
+              v-model="form.research_group_account_id"
+              placeholder="请输入账户编号"
+              size="mini"
+              class="keep-25"
+            >
+            </el-input>
+          </el-form-item>
+          <el-form-item label="金额">
+            <el-input v-model="form.amount" placeholder="请输入金额" size="mini" class="keep-25">
+            </el-input>
+          </el-form-item>
+          <el-form-item label="支付方式">
+            <el-input
+              v-model="form.pay_type"
+              placeholder="请选择支付方式"
+              size="mini"
+              class="keep-25"
+            >
+            </el-input>
+          </el-form-item>
+          <el-form-item label="发票下载地址">
+            <el-input
+              v-model="form.invoice_download_address"
+              placeholder="请输入金额"
+              size="mini"
+              class="keep-25"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="发票日期">
+            <el-date-picker v-model="form.invoice_date" type="date" placeholder="选择日期">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item label="发票图片附件">
+            <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/">
+              <el-button size="small" type="primary">点击上传</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="备注"
+            ><el-input
+              v-model="form.remark"
+              placeholder="请输入金额"
+              size="mini"
+              class="keep-25"
+            ></el-input
+          ></el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -126,7 +194,16 @@ export default {
         pageNo: 1,
         pageSize: 10
       },
-      total: 3
+      total: 3,
+      addDialogVisible: false,
+      form: {
+        id: '',
+        research_group_account_id: '',
+        handler: '',
+        amount: '',
+        pay_type: '',
+        invoice_download_address: ''
+      }
     };
   },
   methods: {
@@ -226,6 +303,10 @@ export default {
     // 判断是否可以取消
     canCancel(row) {
       return row.status === 2; // 只有待付款状态可以取消
+    },
+    // 新增付款记录
+    handleAdd() {
+      this.addDialogVisible = true;
     }
   },
   created() {
@@ -284,5 +365,8 @@ export default {
     text-align: center;
     margin-top: 15px;
   }
+}
+.keep-25 {
+  width: 25%;
 }
 </style>
